f831fec55d6ae43367dad68175072d86110c0d74
Apple Tech <15935486+faluoke@users.noreply.github.com>
2020-03-17 21:12:39 -0600

    set completed on schema to default to false

--- models/todo.js
+++ models/todo.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const todoSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: { type: String, required: true },
-  completed: { type: Boolean, required: true }
+  completed: { type: Boolean, default: false, required: true }
 });
 
 module.exports = mongoose.model("Todo", todoSchema);
