de25a985b47552121337165c7cb09ef81b132ecd
Apple Tech <15935486+faluoke@users.noreply.github.com>
2020-03-17 20:15:10 -0600

    added CRUD endpoints

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -42,4 +42,35 @@ const addTodo = (req, res) => {
     });
 };
 
-module.exports = { getAll, addTodo };
+const updateTodo = (req, res) => {
+  const id = req.params.id;
+  Todo.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+    .then(result => {
+      res.status(200).json({
+        message: "Successfully updated todo item",
+        description: result
+      });
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+};
+
+const deleteTodo = (req, res) => {
+  const id = req.params.id;
+  Todo.findByIdAndDelete(id)
+    .then(result => {
+      if (result) {
+        res.status(200).json({
+          message: "Successfully deleted todo item",
+          description: result
+        });
+      } else {
+        res.status(404).json({ message: "todo item not found" });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({ error: err });
+    });
+};
+module.exports = { getAll, addTodo, updateTodo, deleteTodo };
--- routes/todosRoute.js
+++ routes/todosRoute.js
@@ -1,9 +1,22 @@
 const express = require("express");
 const router = express.Router();
+const controller = require("../controllers/todoController");
 
 /* GET users listing. */
 router.get("/", function(req, res) {
-  res.send("respond with a resource");
+  controller.getAll(req, res);
+});
+
+router.post("/", (req, res) => {
+  controller.addTodo(req, res);
+});
+
+router.put("/:id", (req, res) => {
+  controller.updateTodo(req, res);
+});
+
+router.delete("/:id", (req, res) => {
+  controller.deleteTodo(req, res);
 });
 
 module.exports = router;
